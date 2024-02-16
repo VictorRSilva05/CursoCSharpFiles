724fbb8d4b9009a50b9d0011a38bb065b1b1ac1e
Victor R. Silva <victorr.silva005@gmail.com>
2024-02-16 20:06:18 -0300

    Product processing completed

--- ConsoleApp17/Products.cs
+++ ConsoleApp17/Products.cs
@@ -11,19 +11,28 @@ namespace ConsoleApp17
         public string Name { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
+        public double Total {  get; set; }
 
         public Products() { }
 
-        public Products(string name, double price, int quantity)
+        public Products(string name, double price, int quantity, double total)
         {
             Name = name;
             Price = price;
             Quantity = quantity;
+            Total = total;
         }
 
-        public static double Subtotal(double price, int quantity)
+         public static double Subtotal(double price, int quantity)
         {
-            return price * quantity;
+            return  price * quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name 
+                + ", "
+                + Total.ToString("F2");
         }
     }
 }
--- ConsoleApp17/Program.cs
+++ ConsoleApp17/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace ConsoleApp17
+﻿using System.Globalization;
+using System.IO;
+namespace ConsoleApp17
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            //string path = @"C:\Users\prote\source\repos\products.csv.txt";
+            Console.Write("Please insert file path: ");
+            string path = Console.ReadLine();
+
+            List<Products> products = new List<Products>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string[] lines = File.ReadAllLines(path);
+                    foreach (string line in lines)
+                    {
+                        string[] values = line.Split(',');
+                        string productName = values[0];
+                        double productPrice = double.Parse(values[1], CultureInfo.InvariantCulture);
+                        int productQuantity = int.Parse(values[2], CultureInfo.InvariantCulture);
+                        products.Add(new Products(productName, productPrice, productQuantity, Products.Subtotal(productPrice,productQuantity)));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred.");
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
